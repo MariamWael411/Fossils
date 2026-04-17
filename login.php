e687b848efd7058a9ed41584628e24136976e456
MariamWael411 <dfgsadffg344@gmail.com>
2026-04-17 16:21:07 +0200

    login and register

---
+++ login.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/login.css">
+    <title>statistical website</title>
+</head>
+<body>
+    <div class="background">
+
+<h1 id="topic">Smart Statistical Analyzer</h1>
+
+<p>Advanced Data Analysis Made Easy</p>
+
+    </div>
+
+
+    <div class="login">
+
+<h2>Login</h2>
+
+<form action="">
+<img src="imgs\email (1).png" alt="" id="email" >
+<input type="email" placeholder="Email">
+<br>
+<img src="imgs/padlock.png" alt="" id="password">
+<input type="password" placeholder="Password">
+<br>
+<input type="submit" value="Login" id="submit">
+
+</form>
+<p>Don't have an accoun?</p>
+<a href="register.php">Sign Up</a>
+
+
+    </div>
+
+
+
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/register.css">
+    <title>statistical website</title>
+</head>
+<body>
+    <div class="background">
+
+<h1 id="topic">Smart Statistical Analyzer</h1>
+
+<p>Advanced Data Analysis Made Easy</p>
+
+    </div>
+
+
+    <div class="register">
+
+<h2>Register</h2>
+
+<form action="">
+    <img src="imgs/user.png" alt="" id="people">
+    <input type="text" placeholder="Name">
+    <br>
+<img src="imgs\email (1).png" alt="" id="email" >
+<input type="email" placeholder="Email">
+<br>
+<img src="imgs/padlock.png" alt="" id="password">
+<input type="password" placeholder="Password">
+<br>
+<input type="submit" value="Sign Up" id="submit">
+
+</form>
+<p>Already have an accoun?</p>
+<a href="login.php">Login</a>
+
+
+    </div>
+
+
+
+
+</body>
+</html>
